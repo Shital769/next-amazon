b927b8004f4c10cd3eb63a5c2d2e27f67b076768
Shital769 <110831382+Shital769@users.noreply.github.com>
2024-07-20 23:23:13 +1000

    Listed the product in frontend by creating sample data.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import Header from "@/components/header/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
