0c81015d3424bf59f654393c647e91b5927d6215
Shital769 <110831382+Shital769@users.noreply.github.com>
2024-08-10 22:40:33 +1000

    authenticating user

--- app/(frontend)/signin/SignInForm.tsx
+++ app/(frontend)/signin/SignInForm.tsx
@@ -46,6 +46,52 @@ const SignInForm = () => {
       <div className="max-w-sm mx-auto card bg-base-300 my-4">
          <div className="card-body">
             <h1 className="card-title">Sign In</h1>
+
+            {params.get('error') && (
+               <div className="alert text-error">
+                  {params.get('error') === "CredentialsSignin" ? "Invalid email or password" : params.get('error')
+                  }
+               </div>
+            )}
+
+            {
+               params.get("success") && (
+                  <div className="alert text-success">
+                     {params.get("success")}
+                  </div>
+               )
+            }
+            <form onSubmit={handleSubmit(formSubmit)}>
+               <div className="my-2">
+                  <label htmlFor="email" className="label">Email</label>
+                  <input type="text" id="email" {...register("email", {
+                     required: "Email is required",
+                     pattern: {
+                        value: /[a-z0-9] + @[a-z]+\.[a-z]{2,3}/,
+                        message: "Email is required"
+                     }
+                  })} className="input input-bordered w-full max-w-sm" />
+                  {errors.email?.message && (
+                     <div className="text-error">{errors.email.message}</div>
+                  )}
+               </div>
+               <div className="my-2">
+                  <label htmlFor="password" className="label">Password</label>
+                  <input type="password" id="password" {...register('password', {
+                     required: "Password is required"
+                  })} className="input input-bordered w-full max-w-sm" />
+                  {
+                     errors.password?.message && (
+                        <div className="text-error">{errors.password.message}</div>
+                     )
+                  }
+               </div>
+               <div className="my-4">
+                  <button type="submit" disabled={isSubmitting} className="btn btn-primary w-full" >
+                     {isSubmitting && (<span className="loading loading-spinner"></span>)} Sign In
+                  </button>
+               </div>
+            </form>
          </div>
       </div>
    )
